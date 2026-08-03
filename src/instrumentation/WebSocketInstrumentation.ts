7d394cfca895d2144fa9471ee2e3b85c54aae714
Khalidur Rahman <khalidur.sujon@gmail.com>
2026-08-03 13:34:48 +0600

    refactor: modifify websocket instrumentation to publish events

--- src/instrumentation/WebSocketInstrumentation.ts
+++ src/instrumentation/WebSocketInstrumentation.ts
@@ -1,12 +1,16 @@
-import type { Resource, ResourceIdentity } from '../core';
-import type { Registry } from '../registry';
+import type { ResourceIdentity } from '../core';
+import type {
+  EventPublisher,
+  WebSocketClosedEvent,
+  WebSocketCreatedEvent,
+} from '../events';
 import type { Instrumentation } from './Instrumentation';
 
 export class WebSocketInstrumentation implements Instrumentation {
   private readonly originalWebSocket = globalThis.WebSocket;
   private started = false;
 
-  constructor(private readonly registry: Registry) {}
+  constructor(private readonly publisher: EventPublisher) {}
 
   start(): void {
     if (this.started) {
@@ -16,24 +20,35 @@ export class WebSocketInstrumentation implements Instrumentation {
     this.started = true;
 
     const OriginalWebSocket = this.originalWebSocket;
-    const registry = this.registry;
+    const publisher = this.publisher;
 
     class PatchedWebSocket extends OriginalWebSocket {
       constructor(...args: ConstructorParameters<typeof OriginalWebSocket>) {
         super(...args);
 
-        const resource: Resource = {
-          id: crypto.randomUUID() as ResourceIdentity,
-          type: 'websocket',
-          state: 'observed',
+        const resourceId = crypto.randomUUID() as ResourceIdentity;
+
+        const createdEvent: WebSocketCreatedEvent = {
+          id: crypto.randomUUID(),
+          type: 'WebSocketCreated',
+          timestamp: Date.now(),
+          resourceId,
+          url: args[0].toString(),
         };
 
-        registry.register(resource);
+        publisher.publish(createdEvent);
 
         const originalClose = this.close;
 
         this.close = function (...closeArgs) {
-          registry.release(resource.id);
+          const closedEvent: WebSocketClosedEvent = {
+            id: crypto.randomUUID(),
+            type: 'WebSocketClosed',
+            timestamp: Date.now(),
+            resourceId,
+          };
+
+          publisher.publish(closedEvent);
 
           return originalClose.apply(this, closeArgs);
         };
